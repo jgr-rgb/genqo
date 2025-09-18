d18b1f81d3e9c604c14f7b311a80853f315baade
Gabe Richardson <rgb@umd.edu>
2025-09-18 13:47:14 -0400

    Create proper pytest files in the test folder

--- test/pytest.py
+++
@@ -1,6 +0,0 @@
-# One having run "pip install --upgrade genqo" this script should run without error
-import genqo as gq
-state_test = gq.ZALM()
-state_test.run()
-state_test.calculate_probability_success()
-state_test.results['probability_success']
